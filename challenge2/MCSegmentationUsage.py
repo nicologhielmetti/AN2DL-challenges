f89b5d244e5374f8434c3715e14e4f8c07c8ba21
Nicolò Ghielmetti <nicolo.ghielmetti@gmail.com>
2020-12-15 21:23:33 +0100

    Test class

--- challenge2/MCSegmentationUsage.py
+++ challenge2/MCSegmentationUsage.py
@@ -6,7 +6,7 @@ img_w = 1536
 firstTentative = NeuralNetworkFlow(seed=1996,
                                    dataset_path='/content/Development_Dataset/Training',
                                    n_classes=3,
-                                   out_h=256, out_w=256, img_h=img_h, img_w=1536,
+                                   out_h=256, out_w=256, img_h=img_h, img_w=img_w,
                                    batch_size=32,
                                    n_test_images=15
                                    )
@@ -16,7 +16,7 @@ firstTentative.test_data_generator()
 
 model = firstTentative.create_custom_model(encoder=tf.keras.applications.VGG16(weights='imagenet', include_top=False,
                                                                                input_shape=(img_h, img_w, 3)),
-                                           decoder=firstTentative.create_decoder(depth=2, start_filters=4))
+                                           decoder=firstTentative.create_decoder(depth=5, start_filters=8))
 
 firstTentative.add_neural_network_model(model, firstTentative.create_callbacks(model_name="FIRST_TENTATIVE",
                                                                                save_weights_only=True),
