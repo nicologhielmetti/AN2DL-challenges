bc036060d7b8d127ca53d9e330696e7963ecaa6a
Nicolò Ghielmetti <nicolo.ghielmetti@gmail.com>
2020-12-22 21:27:31 +0100

    Testing

--- challenge2/MCSegmentationUsage.py
+++ challenge2/MCSegmentationUsage.py
@@ -4,13 +4,14 @@ import segmentation_models as sm
 tf.keras.backend.set_image_data_format('channels_last')
 
 
-img_w = 256
-img_h = 256
+img_w = 224
+img_h = 224
 
 # model = sm.Unet('resnet101', classes=3, activation='softmax', input_shape=(img_h, img_w, 3), encoder_weights='imagenet')
 # preproc_f = sm.get_preprocessing('resnet101')
 model = sm.FPN('mobilenetv2', classes=3, activation='softmax', input_shape=(img_h, img_w, 3), encoder_freeze=True)
 preproc_f = sm.get_preprocessing('mobilenetv2')
+model.summary()
 
 firstTentative = NeuralNetworkFlow(seed=1996,
                                    dataset_path='/content/Development_Dataset/Training',
@@ -20,14 +21,14 @@ firstTentative = NeuralNetworkFlow(seed=1996,
                                    n_test_images=15
                                    )
 firstTentative.apply_data_augmentation()
-firstTentative.create_train_validation_sets(preprocessing_function=preproc_f, use_data_aug_test_time=False)
+firstTentative.create_train_validation_sets(preprocessing_function=preproc_f, use_data_aug_test_time=True)
 # firstTentative.test_data_generator()
 
 # model = firstTentative.create_custom_model(encoder=tf.keras.applications.VGG16(weights='imagenet', include_top=False,
 #                                                                                input_shape=(img_h, img_w, 3)),
 #                                            decoder=firstTentative.create_decoder(depth=5, start_filters=32))
 
-firstTentative.add_neural_network_model(model, firstTentative.create_callbacks(model_name="FIRST_TENTATIVE",
+firstTentative.add_neural_network_model(model, firstTentative.create_callbacks(model_name="mobilenetv2",
                                                                                save_weights_only=True,
                                                                                monitor='meanIoU'),
                                         epochs=100,
