d16a4e4c5b346d61bbf865a2e9061aca32af3370
Nicolò Ghielmetti <nicolo.ghielmetti@gmail.com>
2020-12-23 12:57:41 +0100

    Testing

--- challenge2/MCSegmentationUsage.py
+++ challenge2/MCSegmentationUsage.py
@@ -4,8 +4,8 @@ import segmentation_models as sm
 tf.keras.backend.set_image_data_format('channels_last')
 
 
-img_w = 512
-img_h = 256
+img_w = 300
+img_h = 300
 
 # model = sm.Unet('resnet101', classes=3, activation='softmax', input_shape=(img_h, img_w, 3), encoder_weights='imagenet')
 # preproc_f = sm.get_preprocessing('resnet101')
